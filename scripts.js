121386d4719f0bfd834861c93fb4f041af3b02a4
Teratesh1922 <zaka30207@gmail.com>
2025-05-06 16:01:54 +0700

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
-document.getElementById('changeTextButton').addEventListener('click', function() {
-  document.querySelector('header h1').textContent = "Teks telah diubah!";
-});
+const dummyUser = {
+  username: "admin",
+  password: "123456"
+};
+
+function login() {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username === dummyUser.username && password === dummyUser.password) {
+    localStorage.setItem("isLoggedIn", "true");
+    window.location.href = "home.html";
+  } else {
+    document.getElementById("error-msg").innerText = "Username atau password salah!";
+  }
+}
